46b87c19afd8f04daee9fbe0e335c73241ee7a0a
abdo <abdo.taleb1@hotmail.com>
2023-02-28 23:42:54 +0100

    Initial commit

---
+++ src/main/java/com/example/movies/model/Character.java
@@ -0,0 +1,31 @@
+package com.example.movies.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_character")
+public class Character {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "movie_id")
+//    private int id;
+//    @Column(name = "movie_title", length = 50, nullable = false)
+//    private String title;
+//
+//    @Column(name = "movie_genre", length = 50)
+//    private String genre;
+//
+//    @Column(name = "movie_release_date", length = 50, nullable = false)
+//    private int release_date;
+//
+//    @Column(name = "movie_director", length = 50)
+//    private String director;
+//
+//    @Column(name = "movie_poster", length = 200, nullable = false)
+//    private String poster;
+//
+//    @Column(name = "movie_trailer", length = 200, nullable = false)
+//    private String trailer;
+
+    // Getters and setters
+}
